fd1bc1f80af99e4aa985b204dc62e158dd95b11c
imekachi <imekachi@gmail.com>
2021-01-22 13:47:02 +0700

    migrate to typescript

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,17 @@
  * @param {any[][]} variantArr
  * @return {any[][]} combinations
  */
-export default function combinations(variantArr) {
+export default function combinations(variantArr: any[][]) {
+  // I'm gonna explain this using an example.
+  // Let's say we have a shirt shop
+  // and customer can choose options in 2 variants
+  // - color: red, green, blue
+  // - size: S, M, L
+  // We're gonna find out all possible combinations of our shirts
+  //
   let buffer = []
 
+  // Main loop, loop through each optionArr
   variantArr.forEach((optionArr) => {
     if (!buffer.length) {
       buffer = optionArr.map((item) => [item])
